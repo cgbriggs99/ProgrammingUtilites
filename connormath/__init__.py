76c542ca03571cacaa5e0fe7562c26713d1e738a
Connor Briggs <cgbriggs99@gmail.com>
2022-05-04 10:18:08 -0400

    Changing python backend

--- connormath/__init__.py
+++ connormath/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-from .funcs import *
-from .ints import *
+#from .funcs import *
+#from .ints import *
 from .roots import *
-from .stats import *
+from .roots.croots import *
+#from .stats import *
